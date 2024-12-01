ce9f4b3594550e5f2232235a416b86ecc05fc0ff
Siddartha Tummala <sidddddd@amazon.com>
2024-12-01 15:04:22 -0800

    Introduced functionality to send and read multiple time-spaced requests over the same connection

--- main.go
+++ main.go
@@ -4,32 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
+	"time"
 )
 
+func handleConnection(conn *net.Conn, index int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// Send a request
+	request := []byte("This is a request from the star-db client\n")
+	requestBytes, err := (*conn).Write(request)
+	if err != nil {
+		fmt.Println("Error sending a request to the star-db server: ", request)
+		log.Fatal(err)
+	}
+	fmt.Printf("Sent %d bytes to the star-db server. index: %d \n", requestBytes, index)
+
+	// Read the response
+	buf := make([]byte, 1024)
+	responseBytes, err := (*conn).Read(buf)
+	if err != nil {
+		fmt.Println("Error reading the response from the star-db server: ", err)
+		log.Fatal(err)
+	}
+	fmt.Printf("index response: %d \n.", index)
+	fmt.Println("Response from star-db server: \n", string(buf[:responseBytes]))
+}
+
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
+	conn, err := net.Dial("tcp", ":8080")
 	if err != nil {
 		fmt.Println("Error opening a connection to the star-db server")
 		log.Fatal(err)
 	}
 	defer conn.Close()
-
-	// Send a request
-	request := []byte("This is a request from the star-db client\n")
-	requestBytes, err := conn.Write(request)
-	if err != nil {
-		fmt.Println("Error sending a request to the star-db server: ", request)
-		log.Fatal(err)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go handleConnection(&conn, i, &wg)
+		time.Sleep(1 * time.Second)
 	}
-	fmt.Printf("Sent %d bytes to the star-db server", requestBytes)
-
-	// Read the response
-	buf := make([]byte, 1024)
-	responseBytes, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading the response from the star-db server: ", err)
-		log.Fatal(err)
-	}
-
-	fmt.Println("Response from star-db server: ", string(buf[:responseBytes]))
+	wg.Wait()
+	conn.Close()
 }
